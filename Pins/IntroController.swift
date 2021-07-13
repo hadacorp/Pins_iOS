f186a482d174e3d5c6fe0096fe17899421299c71
judongseok <wnehdtjr5@naver.com>
2021-07-13 22:17:54 +0900

    Add IntroView & LoginView

---
+++ Pins/IntroController.swift
@@ -0,0 +1,29 @@
+import UIKit
+
+class IntroController: UIViewController {
+
+    @IBOutlet weak var firstLoginButton: UIButton!
+    @IBOutlet weak var exsitingLoginButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        loginButtonStyle()
+    }
+    
+    // MARK: - View Style
+    func loginButtonStyle(){
+        firstLoginButton.layer.cornerRadius = 10
+        exsitingLoginButton.layer.cornerRadius = 10
+    }
+    
+    
+    // MARK: - Button Listenr
+    @IBAction func firstLoginButtonListener(_ sender: Any) {
+        let vcLogin = self.storyboard?.instantiateViewController(identifier: "vcLogin")
+        vcLogin?.modalPresentationStyle = .fullScreen
+        present(vcLogin!, animated: true, completion: nil)
+    }
+}
+
