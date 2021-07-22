0098584206d4913c983b49023606e2ac07af4e60
MojitoBar <wnehdtjr5@gmail.com>
2021-07-22 23:14:12 +0900

    MVVM pattern apply

--- Pins/View/MainViewController.swift
+++ Pins/View/MainViewController.swift
@@ -2,38 +2,28 @@ import UIKit
 
 class MainViewController: UIViewController {
 
-    
-    // 화면 스크린 가로 세로 사이즈
-    private var SCREEN_WIDTH = UIScreen.main.bounds.width
-    private var SCREEN_HEIGHT = UIScreen.main.bounds.height
-    
-    
+    // MARK:- @IBOutlet Properties
     // 메인 뷰 배너 아울렛
     @IBOutlet weak var mainViewBanner: UICollectionView!
     
-   
-    // 배너 이미지 목록 리스트
-    var bannerImageArray = [
-        UIImage(named: "mainBanner"),
-        UIImage(named: "mainBanner"),
-        UIImage(named: "mainBanner"),
-        UIImage(named: "mainBanner"),
-        UIImage(named: "mainBanner"),
-        UIImage(named: "mainBanner")
-    ]
+    // MARK:- Private Properties
+    // 뷰 모델 초기화
+    private let viewModel = MainViewModel()
     // 메인 뷰 배너 페이지 카운트
-    var nowPage: Int = 0
+    private var nowPage: Int = 0
     
-    
+    // MARK:- Functions
     override func viewDidLoad() {
         super.viewDidLoad()
         // 델리게이트와 데이터 소스를 어디서 처리할 것인지 작성
         mainViewBanner.delegate = self
         mainViewBanner.dataSource = self
         
+        // 배너 자동 스크롤 타이머 적용
         bannerTimer()
     }
     
+    // MARK:- Banner func
     // 배너 타이머
     func bannerTimer() {
         let _: Timer = Timer.scheduledTimer(withTimeInterval: 4.5, repeats: true) { (Timer) in
@@ -43,7 +33,7 @@ class MainViewController: UIViewController {
     // 배너 움직이는 매서드
     func bannerMove() {
         // 현재페이지가 마지막 페이지일 경우
-        if nowPage == bannerImageArray.count-1 {
+        if nowPage == viewModel.numOfBannerImageList - 1 {
         // 맨 처음 페이지로 돌아감
             mainViewBanner.scrollToItem(at: NSIndexPath(item: 0, section: 0) as IndexPath, at: .right, animated: true)
             nowPage = 0
@@ -54,16 +44,19 @@ class MainViewController: UIViewController {
         mainViewBanner.scrollToItem(at: NSIndexPath(item: nowPage, section: 0) as IndexPath, at: .right, animated: true)
     }
 }
+
+// MARK:- Extensions
+// MARK:- CollectionView Setting
 extension MainViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
     //컬렉션뷰 개수 설정
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return bannerImageArray.count
+        return viewModel.numOfBannerImageList
     }
     
     //컬렉션뷰 셀 설정
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = mainViewBanner.dequeueReusableCell(withReuseIdentifier: "BannerCell", for: indexPath) as! BannerCell
-        cell.imageView.image = bannerImageArray[indexPath.row]
+        cell.imageView.image = viewModel.bannerImageList[indexPath.row].image
         
         return cell
     }
@@ -71,8 +64,6 @@ extension MainViewController: UICollectionViewDelegate, UICollectionViewDataSour
     // UICollectionViewDelegateFlowLayout 상속
     //컬렉션뷰 사이즈 설정
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        var w = mainViewBanner.frame.width
-        var h = mainViewBanner.frame.height
         return CGSize(width: mainViewBanner.frame.width, height: mainViewBanner.frame.height)
     }
     
