9ce4f16b95b881c5cdf0a13c19d3a2d517b227a8
judongseok <wnehdtjr5@gmail.com>
2021-08-01 02:08:41 +0900

    Wrapped MainView With ScrollView

--- Pins/View/HomeView/JoinCard/JoinScrollView.swift
+++ Pins/View/HomeView/JoinCard/JoinScrollView.swift
@@ -30,31 +30,3 @@ class JoinScrollView{
         scrollView.showsHorizontalScrollIndicator = false
     }
 }
-extension MainViewController{
-    func simillerValue() -> Int{
-        var array: [Int] = []
-        for i in 0 ..< viewModel.joinCardList.count{
-            array.append((Int(UIScreen.main.bounds.width - 24) * i))
-        }
-        var less = Int.max
-        var goal = 0
-        for i in 0 ..< array.count{
-            let value = abs(Int(scrollView.contentOffset.x) - array[i])
-            if less > value{
-                less = value
-                goal = array[i]
-            }
-        }
-        return goal
-    }
-    
-    func scrollViewDidScroll(_ scrollView: UIScrollView) {
-        if(scrollView == self.scrollView && scrollView.isDecelerating == true){
-            let goal = simillerValue()
-            UIView.animate(withDuration: 0.5) {
-                print("anime to" + String(goal))
-                scrollView.scrollRectToVisible(CGRect(x: CGFloat(goal), y: 0, width: scrollView.bounds.size.width, height: scrollView.bounds.size.height), animated: false)
-            }
-        }
-    }
-}
