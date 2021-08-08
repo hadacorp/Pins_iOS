f03b87b97a051965e07adcb56918e30590b3db76
judongseok <wnehdtjr5@gmail.com>
2021-08-08 18:16:24 +0900

    Newest Project Init

---
+++ newest/pins_ios/pins_ios/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  pins_ios
+//
+//  Created by judongseok on 2021/08/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
