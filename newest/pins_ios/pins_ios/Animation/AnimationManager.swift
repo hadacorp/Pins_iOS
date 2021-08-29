fb177faba3d4eb3fc650dd0a2df58c8b536c498d
judongseok <wnehdtjr5@gmail.com>
2021-08-29 23:40:21 +0900

    Add First Button Animation

---
+++ newest/pins_ios/pins_ios/Animation/AnimationManager.swift
@@ -0,0 +1,16 @@
+//
+//  AnimationManager.swift
+//  pins_ios
+//
+//  Created by judongseok on 2021/08/29.
+//
+
+import UIKit
+
+class AnimationManager {
+    @objc func filterAnimate(){
+        UIView.animate(withDuration: 1) {
+            
+        }
+    }
+}
