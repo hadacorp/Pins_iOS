985a015a32289206ca15a91d1a8dfdf8acba8481
judongseok <wnehdtjr5@gmail.com>
2021-09-03 01:47:02 +0900

    Add Gradation Up&Down

--- newest/pins_ios/viewController/SearchViewController.swift
+++ newest/pins_ios/viewController/SearchViewController.swift
@@ -10,15 +10,18 @@ import UIKit
 class SearchViewController: UIViewController {
     @IBOutlet weak var textField: UITextField!
     @IBAction func backButton(_ sender: Any) {
-        let transition = CATransition()
-        transition.duration = 0.25
-        transition.type = CATransitionType.push
-        transition.subtype = CATransitionSubtype.fromLeft
-        self.view.window!.layer.add(transition, forKey: kCATransition)
+//        let transition = CATransition()
+//        transition.duration = 0.25
+//        transition.type = CATransitionType.push
+//        transition.subtype = CATransitionSubtype.fromLeft
+//        self.view.window!.layer.add(transition, forKey: kCATransition)
         
-        dismiss(animated: false)
+        dismiss(animated: true)
     }
     override func viewDidLoad() {
+        // 라이트 모드로 고정
+        self.overrideUserInterfaceStyle = .light
+        textField.becomeFirstResponder()
         setUI()
     }
     
@@ -28,5 +31,11 @@ class SearchViewController: UIViewController {
         textField.layer.cornerRadius = 16
         textField.layer.borderColor = #colorLiteral(red: 0.3764705882, green: 0.3764705882, blue: 0.3764705882, alpha: 1)
         textField.layer.borderWidth = 1
+        textField.tintColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+        textField.font = UIFont(name: "NotoSansKR-Regular", size: 16)
+        
+        let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: 40, height: textField.frame.height))
+        textField.leftView = paddingView
+        textField.leftViewMode = .always
     }
 }
