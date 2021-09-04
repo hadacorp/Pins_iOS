c3c43867a739a8bb18a7a47f0db4f4d530ec8d51
judongseok <wnehdtjr5@gmail.com>
2021-09-04 20:53:43 +0900

    Add LocalSearch

---
+++ newest/pins_ios/Entities/Cell.swift
@@ -0,0 +1,14 @@
+//
+//  Cell.swift
+//  pins_ios
+//
+//  Created by judongseok on 2021/09/04.
+//
+
+import UIKit
+
+class Cell: UITableViewCell {
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var iconImage: UIImageView!
+    @IBOutlet weak var distance: UILabel!
+}
