b4d8f130dce7de126b530ed15a4c12fb9a5fd574
judongseok <wnehdtjr5@gmail.com>
2021-09-20 02:00:33 +0900

    [#2] Feat Delete CoreData Function

--- newest/pins_ios/pins_ios/Utill/CoreDataManager.swift
+++ newest/pins_ios/pins_ios/Utill/CoreDataManager.swift
@@ -23,14 +23,14 @@ class CoreDataManager {
     // MARK: - 해당 정보를 저장한다
     func saveRecentSearch(term: String, index: Int16, completion: @escaping (Bool) -> Void) {
         guard let context = self.context,
-            let entity = NSEntityDescription.entity(forEntityName: CoreDataName.recentResearch.rawValue, in: context)
-            else { return }
-
+              let entity = NSEntityDescription.entity(forEntityName: CoreDataName.recentResearch.rawValue, in: context)
+        else { return }
+        
         guard let recentTerms = NSManagedObject(entity: entity, insertInto: context) as? RecentResearchTerm else { return }
         
         recentTerms.term = term
         recentTerms.index = index
-
+        
         do {
             try context.save()
             completion(true)
@@ -59,5 +59,40 @@ class CoreDataManager {
         }
         return models
     }
+    // MARK: - 해당 정보를 삭제한다.
+    func deleteUser(id: Int16, onSuccess: @escaping ((Bool) -> Void)) {
+        let fetchRequest: NSFetchRequest<NSManagedObject> = NSFetchRequest<NSManagedObject>(entityName: CoreDataName.recentResearch.rawValue)
+        do {
+            if let results: [RecentResearchTerm] = try context!.fetch(fetchRequest) as? [RecentResearchTerm] {
+                if results.count != 0 {
+                    context?.delete(results[0])
+                }
+            }
+        } catch let error as NSError {
+            print("Could not fatch🥺: \(error), \(error.userInfo)")
+            onSuccess(false)
+        }
+        contextSave { success in
+            onSuccess(success)
+        }
+    }
 }
 
+extension CoreDataManager {
+    fileprivate func filteredRequest(id: Int16) -> NSFetchRequest<NSFetchRequestResult> {
+        let fetchRequest: NSFetchRequest<NSFetchRequestResult>
+            = NSFetchRequest<NSFetchRequestResult>(entityName: CoreDataName.recentResearch.rawValue)
+        fetchRequest.predicate = NSPredicate(format: "index = %@", NSNumber(value: id))
+        return fetchRequest
+    }
+    
+    fileprivate func contextSave(onSuccess: ((Bool) -> Void)) {
+        do {
+            try context?.save()
+            onSuccess(true)
+        } catch let error as NSError {
+            print("Could not save🥶: \(error), \(error.userInfo)")
+            onSuccess(false)
+        }
+    }
+}
