1bf438527d1e81b3a0440bdc4f5a81c811eb5263
judongseok <wnehdtjr5@gmail.com>
2021-10-05 02:30:38 +0900

    Feat MeetingFilter View

--- newest/pins_ios/pins_ios/viewController/FilterController.swift
+++ newest/pins_ios/pins_ios/viewController/FilterController.swift
@@ -8,5 +8,17 @@
 import UIKit
 
 class FilterController: UIViewController {
-
+    
+    @IBAction func communityFilterBtn(_ sender: Any) {
+        print("커뮤니티 필터")
+    }
+    @IBAction func storyFilterBtn(_ sender: Any) {
+        print("스토리 필터")
+    }
+    @IBAction func backFilterBtn(_ sender: Any) {
+        print("back")
+        let VC1 = self.storyboard!.instantiateViewController(withIdentifier: "MainVC") as! ViewController
+        
+        self.navigationController!.pushViewController(VC1, animated: false)
+    }
 }
