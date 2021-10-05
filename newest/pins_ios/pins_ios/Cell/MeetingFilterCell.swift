e9234af1c2884732c375afa0c560d440adf0b1c0
judongseok <wnehdtjr5@gmail.com>
2021-10-06 02:28:36 +0900

    Modified FilterView CollectionView Click Error

--- newest/pins_ios/pins_ios/Cell/MeetingFilterCell.swift
+++ newest/pins_ios/pins_ios/Cell/MeetingFilterCell.swift
@@ -8,4 +8,15 @@
 import UIKit
 
 class MeetingFilterCell: UICollectionViewCell {
+    override init(frame: CGRect){
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setupCell(color: UIColor){
+        self.backgroundColor = color
+    }
 }
