8794167bc81c0fdd71129a6e6989fe989e42c8ff
judongseok <wnehdtjr5@gmail.com>
2021-10-06 02:28:36 +0900

    Modified FilterView CollectionView Click Error

--- newest/pins_ios/pins_ios/viewController/MeetingFilterController.swift
+++ newest/pins_ios/pins_ios/viewController/MeetingFilterController.swift
@@ -8,8 +8,6 @@
 import UIKit
 
 class MeetingFilterController: UIViewController {
-    @IBOutlet weak var collectionView: UICollectionView!
-    
     @IBAction func communityFilterBtn(_ sender: Any) {
         let VC1 = self.storyboard!.instantiateViewController(withIdentifier: "CommunityFilterVC") as! CommunityFilterController
         
@@ -26,13 +24,36 @@ class MeetingFilterController: UIViewController {
             }
         }
     }
-    private var viewModel = MeetingFilterViewModel()
+    public var collectionView: UICollectionView!
     
-    private let sectionInsets = UIEdgeInsets(top: 0, left: 8, bottom: 0, right: 8)
+    private var viewModel = MeetingFilterViewModel()
       
     override func viewDidLoad() {
-        collectionView.delegate = self
+        setCollectionView()
+    }
+    
+    private func setCollectionView(){
+        let layout: UICollectionViewFlowLayout = UICollectionViewFlowLayout()
+        layout.sectionInset = UIEdgeInsets(top: 0, left: 4, bottom: 0, right: 4)
+        layout.itemSize = CGSize(width: Int((UIScreen.main.bounds.width - 48) / 3), height: 28)
+        layout.minimumLineSpacing = 24
+        layout.scrollDirection = .vertical
+        
+        collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
         collectionView.dataSource = self
+        collectionView.delegate = self
+        collectionView.register(MeetingFilterCell.self, forCellWithReuseIdentifier: "MeetingFilterCell")
+        collectionView.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0)
+        collectionView.decelerationRate = UIScrollView.DecelerationRate.fast
+        collectionView.showsHorizontalScrollIndicator = false
+        self.view.addSubview(collectionView)
+        collectionView.snp.makeConstraints { view in
+            view.width.equalTo(UIScreen.main.bounds.width)
+            view.height.equalTo(208)
+            view.top.equalTo(self.view.safeAreaInsets).offset(184)
+            view.leading.equalTo(10)
+            view.trailing.equalTo(-10)
+        }
     }
 }
 
@@ -42,25 +63,31 @@ extension MeetingFilterController: UICollectionViewDelegate, UICollectionViewDat
     }
     
     public func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "meetingFilterCell", for: indexPath) as! MeetingFilterCell
-        
-        let title = UILabel(frame: CGRect(x: 0, y: 0, width: cell.bounds.width, height: 28))
-        title.text = viewModel.getFilters()[indexPath.row]
-        if viewModel.getClickList()[indexPath.row] == 0{
-            title.textColor = #colorLiteral(red: 0.4520480633, green: 0.4520593286, blue: 0.4520532489, alpha: 1)
-            title.backgroundColor = #colorLiteral(red: 0.9625374675, green: 0.9625598788, blue: 0.9625478387, alpha: 1)
+        if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "MeetingFilterCell", for: indexPath) as? MeetingFilterCell {
+            for v in cell.subviews{
+                v.removeFromSuperview()
+            }
+            
+            let title = UILabel(frame: CGRect(x: 0, y: 0, width: cell.bounds.width, height: 28))
+            cell.addSubview(title)
+            cell.setupCell(color: .white)
+            if viewModel.getClickList()[indexPath.row] == 0{
+                title.textColor = #colorLiteral(red: 0.4520480633, green: 0.4520593286, blue: 0.4520532489, alpha: 1)
+                title.backgroundColor = #colorLiteral(red: 0.9625374675, green: 0.9625598788, blue: 0.9625478387, alpha: 1)
+            }
+            else{
+                title.textColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+                title.backgroundColor = #colorLiteral(red: 0.1137254902, green: 0.6666666667, blue: 0.9529411765, alpha: 1)
+            }
+            title.text = viewModel.getFilters()[indexPath.row]
+            title.font = UIFont(name: "NotoSansKR-Regular", size: 13)
+            title.textAlignment = .center
+            title.clipsToBounds = true
+            title.layer.cornerRadius = 14
+            
+            return cell
         }
-        else{
-            title.textColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
-            title.backgroundColor = #colorLiteral(red: 0.1137254902, green: 0.6666666667, blue: 0.9529411765, alpha: 1)
-        }
-        title.font = UIFont(name: "NotoSansKR-Regular", size: 13)
-        title.textAlignment = .center
-        title.clipsToBounds = true
-        title.layer.cornerRadius = 14
-        
-        cell.contentView.addSubview(title)
-        return cell
+        fatalError("Unable to dequeue subclassed cell")
     }
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
@@ -69,25 +96,3 @@ extension MeetingFilterController: UICollectionViewDelegate, UICollectionViewDat
     }
 }
 
-extension MeetingFilterController: UICollectionViewDelegateFlowLayout {
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
-        return 0
-    }
-    // 상하 여백
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
-        return 24
-    }
-    // cell 사이즈( 옆 라인을 고려하여 설정 )
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        
-        let width = (collectionView.frame.width - 48) / 3
-        let size = CGSize(width: width, height: 28)
-        return size
-    }
-    
-    func collectionView(_ collectionView: UICollectionView,
-                        layout collectionViewLayout: UICollectionViewLayout,
-                        insetForSectionAt section: Int) -> UIEdgeInsets {
-        return sectionInsets
-    }
-}
