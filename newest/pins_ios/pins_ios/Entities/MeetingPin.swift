1b2c8b31e176a1ce9e746837cfe8ae33ee10c03b
judongseok <wnehdtjr5@gmail.com>
2021-10-08 00:53:08 +0900

    Feat pin choice view 90%

---
+++ newest/pins_ios/pins_ios/Entities/MeetingPin.swift
@@ -0,0 +1,27 @@
+//
+//  MeetingPin.swift
+//  pins_ios
+//
+//  Created by judongseok on 2021/10/08.
+//
+
+import UIKit
+
+class MeetingPin {
+    static let shared = MeetingPin()
+    
+    var title: String?
+    var content: String?
+    var category: String?
+    var setGender: String?
+    var minAge: Int?
+    var maxAge: Int?
+    var setLimit: Int?
+    var longitude: Double?
+    var latitude: Double?
+    var date: Int?
+    var hour: Int?
+    var minute: Int?
+    
+    private init() {}
+}
