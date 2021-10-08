f1a5652375e99e8280447907ca20732183f3e497
judongseok <wnehdtjr5@gmail.com>
2021-10-09 00:23:30 +0900

    Feat meetingPin click date logic

--- newest/pins_ios/pins_ios/Cell/MeetingDateCell.swift
+++ newest/pins_ios/pins_ios/Cell/MeetingDateCell.swift
@@ -16,10 +16,10 @@ class MeetingDateCell: UICollectionViewCell {
         fatalError("init(coder:) has not been implemented")
     }
     
-    func setupCell(color: UIColor, text: String){
+    func setupCell(textColor: UIColor, backColor: UIColor, text: String){
         let label = UILabel()
         label.text = text
-        label.textColor = #colorLiteral(red: 0.3764705882, green: 0.3764705882, blue: 0.3764705882, alpha: 1)
+        label.textColor = textColor
         label.numberOfLines = 2
         label.font = UIFont(name: "NotoSansKR-Regular", size: 14)
         label.textAlignment = .center
@@ -31,6 +31,6 @@ class MeetingDateCell: UICollectionViewCell {
             make.height.equalTo(56)
         }
         self.layer.cornerRadius = 8
-        self.backgroundColor = color
+        self.backgroundColor = backColor
     }
 }
