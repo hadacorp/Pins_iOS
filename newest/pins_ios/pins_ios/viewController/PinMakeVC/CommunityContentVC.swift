c9ad7c955456d4e57633e068103efd182b003f84
MojitoBar <wnehdtjr5@gmail.com>
2021-10-31 03:25:28 +0900

    Feat textFiled in communityContentVC

--- newest/pins_ios/pins_ios/viewController/PinMakeVC/CommunityContentVC.swift
+++ newest/pins_ios/pins_ios/viewController/PinMakeVC/CommunityContentVC.swift
@@ -11,6 +11,10 @@ class CommunityContentVC: UIViewController {
     @IBAction func backBtn(_ sender: Any) {
         self.navigationController?.popViewController(animated: false)
     }
+    @IBOutlet weak var textViewName: UITextView!
+    @IBOutlet weak var textViewContent: UITextView!
+    @IBOutlet weak var textViewRule: UITextView!
+    
     @IBOutlet weak var successBtn: UIButton!
     @IBAction func successBtn(_ sender: Any) {
         // API 호출
@@ -25,6 +29,25 @@ class CommunityContentVC: UIViewController {
         initImageView()
         createTriangle()
         successBtn.setTitleColor(#colorLiteral(red: 0.9215686275, green: 0.9215686275, blue: 0.9215686275, alpha: 1), for: .normal)
+        
+        setTextView()
+    }
+    private func setTextView(){
+        textViewName.delegate = self
+        textViewContent.delegate = self
+        textViewRule.delegate = self
+        
+        textViewName.tag = 1
+        textViewContent.tag = 2
+        textViewRule.tag = 3
+        
+        textViewName.textColor = #colorLiteral(red: 0.6, green: 0.6, blue: 0.6, alpha: 1)
+        textViewContent.textColor = #colorLiteral(red: 0.6, green: 0.6, blue: 0.6, alpha: 1)
+        textViewRule.textColor = #colorLiteral(red: 0.6, green: 0.6, blue: 0.6, alpha: 1)
+        
+        textViewName.textContainerInset = .zero
+        textViewContent.textContainerInset = .zero
+        textViewRule.textContainerInset = .zero
     }
     
     private func initImageView(){
@@ -41,6 +64,7 @@ class CommunityContentVC: UIViewController {
         
         initImageViewIcon(parent: btn)
         initImageViewText(parent: btn)
+        btn.addTarget(self, action: #selector(clickImageBtn), for: .touchUpInside)
     }
     private func initImageViewIcon(parent: UIView){
         let img = UIImageView(image: #imageLiteral(resourceName: "iconCamera"))
@@ -85,5 +109,51 @@ class CommunityContentVC: UIViewController {
             make.top.equalTo(self.view.safeAreaLayoutGuide).offset(66)
             make.trailing.equalTo(-UIScreen.main.bounds.width / 3 + 2)
         }
+    }
+    
+    @objc func clickImageBtn(){
+        print("Clicked")
+    }
+}
+
+extension CommunityContentVC: UITextViewDelegate{
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        if textView.textColor == #colorLiteral(red: 0.6, green: 0.6, blue: 0.6, alpha: 1) {
+            textView.text = nil
+            textView.textColor = UIColor.black
+        }
+    }
+    
+    func textViewDidEndEditing(_ textView: UITextView) {
+        if textView.tag == 1 && textView.text.isEmpty {
+            textView.text = "커뮤니티명(4~12글자)"
+            textView.textColor = #colorLiteral(red: 0.6, green: 0.6, blue: 0.6, alpha: 1)
+        }
+        else if textView.tag == 2 && textView.text.isEmpty {
+            textView.text = "커뮤니티 소개(4~30글자)"
+            textView.textColor = #colorLiteral(red: 0.6, green: 0.6, blue: 0.6, alpha: 1)
+        }
+        else if textView.tag == 3 && textView.text.isEmpty {
+            textView.text = "자세한 소개, 규칙, 조건 등을 적어주세요"
+            textView.textColor = #colorLiteral(red: 0.6, green: 0.6, blue: 0.6, alpha: 1)
+        }
+    }
+    func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+        if textView.tag == 1{
+            let newLength = textView.text!.count + text.count - range.length
+            
+            return !(newLength >= 12)
+        }
+        else if textView.tag == 2{
+            let newLength = textView.text!.count + text.count - range.length
+
+            return !(newLength >= 30)
+        }
+        else if textView.tag == 3{
+            let newLength = textView.text!.count + text.count - range.length
+
+            return !(newLength >= 300)
+        }
+        return true
     }
 }
