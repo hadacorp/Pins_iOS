63bdb83531414d238f194dce887a17a5171c105e
judongseok <wnehdtjr5@naver.com>
2021-10-31 23:28:35 +0900

    Feat postCommunityPin function

---
+++ newest/pins_ios/pins_ios/API/PostCommunityPin.swift
@@ -0,0 +1,65 @@
+//
+//  PostCommunityPin.swift
+//  pins_ios
+//
+//  Created by judongseok on 2021/10/31.
+//
+
+import UIKit
+
+class PostCommunityPin {
+    func uploadImage(paramName: String, fileName: String, image: UIImage) {
+        let parameters = [
+            [
+                "key": "key",
+                "value": "value",
+                "type": "text"
+            ]] as [[String : Any]]
+        
+        let boundary = "Boundary-\(UUID().uuidString)"
+        var body = ""
+        for param in parameters {
+            if param["disabled"] == nil {
+                let paramName = param["key"]!
+                body += "--\(boundary)\r\n"
+                body += "Content-Disposition:form-data; name=\"\(paramName)\""
+                if param["contentType"] != nil {
+                    body += "\r\nContent-Type: \(param["contentType"] as! String)"
+                }
+                let paramType = param["type"] as! String
+                if paramType == "text" {
+                    let paramValue = param["value"] as! String
+                    body += "\r\n\r\n\(paramValue)\r\n"
+                } else {
+                    let paramSrc = param["src"] as! String
+                    let fileData = try? NSData(contentsOfFile:paramSrc, options:[]) as Data
+                    let fileContent = String(data: fileData!, encoding: .utf8)!
+                    body += "; filename=\"\(paramSrc)\"\r\n"
+                    + "Content-Type: \"content-type header\"\r\n\r\n\(fileContent)\r\n"
+                }
+            }
+        }
+        body += "--\(boundary)--\r\n";
+        let postData = body.data(using: .utf8)
+        
+        var request = URLRequest(url: URL(string: "https://apitest....")!,timeoutInterval: Double.infinity)
+        request.addValue("multipart/form-data; boundary=\(boundary)", forHTTPHeaderField: "Content-Type")
+        request.addValue("eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIwMTAtNzc2MC02MzkzIiwicm9sZXMiOlsiUk9MRV9VU0VSIl0sImV4cCI6MTY2MTYxNDI4OH0.Ojb-VgKgoXJSB5Y9u9-165Y2VwLNuP1Pv-KbDeYt_Yg", forHTTPHeaderField: "X-AUTH-TOKEN")
+        
+        request.httpMethod = "POST"
+        request.httpBody = postData
+        
+        print(request)
+        dump(request)
+        dump(request.httpBody)
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            guard let data = data else {
+                print(String(describing: error))
+                return
+            }
+            print(String(data: data, encoding: .utf8)!)
+        }
+        
+        task.resume()
+    }
+}
