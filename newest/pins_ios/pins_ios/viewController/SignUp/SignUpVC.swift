b63baa75e5a065a5e529ed84e346a1ee443eefed
judongseok <wnehdtjr5@naver.com>
2021-11-22 00:20:17 +0900

    Feat backbtn

--- newest/pins_ios/pins_ios/viewController/SignUp/SignUpVC.swift
+++ newest/pins_ios/pins_ios/viewController/SignUp/SignUpVC.swift
@@ -8,6 +8,7 @@
 import UIKit
 
 class SignUpVC: UIViewController, BaseViewController{
+    // MARK: -ViewController LifeCycle
     override func viewDidLoad() {
         setUI()
     }
@@ -16,11 +17,21 @@ class SignUpVC: UIViewController, BaseViewController{
         self.nameTextField.becomeFirstResponder()
     }
     
+    // MARK: -Initial UI
+    
     let nameLine: UIView = {
         let line = UIView()
         
         line.backgroundColor = #colorLiteral(red: 0.9215686275, green: 0.9215686275, blue: 0.9215686275, alpha: 1)
         return line
+    }()
+    
+    let backBtn: UIButton = {
+        let btn = UIButton()
+        
+        btn.setImage(UIImage(named: "btnBack"), for: .normal)
+        btn.addTarget(self, action: #selector(backAction), for: .touchUpInside)
+        return btn
     }()
     
     let phoneNumberPlaceholder: UILabel = {
@@ -75,83 +86,18 @@ class SignUpVC: UIViewController, BaseViewController{
         button.backgroundColor = #colorLiteral(red: 0.1137254902, green: 0.6666666667, blue: 0.9529411765, alpha: 1)
         return button
     }()
+    
+    let mainTitle: UILabel = {
+        let title = UILabel()
+        
+        title.text = "이름을 입력해 주세요."
+        title.font = UIFont(name: "NotoSansKR-Medium", size: 20)
+        title.textColor = #colorLiteral(red: 0.06666666667, green: 0.06666666667, blue: 0.06666666667, alpha: 1)
+        return title
+    }()
 }
 
-extension SignUpVC{
-    @objc
-    func keyboardWillShow(_ sender: Notification) {
-        if let keyboardFrame: NSValue = sender.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue {
-            let keybaordRectangle = keyboardFrame.cgRectValue
-            let keyboardHeight = keybaordRectangle.height
-            
-            nameSuccess.snp.makeConstraints { make in
-                make.bottom.equalTo(-keyboardHeight)
-            }
-        }
-    }
-    
-     func setUI(){
-        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
-        addSubViews()
-        setLayout()
-        setDelegate()
-    }
-    
-    func setDelegate(){
-        nameTextField.delegate = self
-    }
-    
-    func addSubViews(){
-        view.addSubview(nameTextField)
-        nameTextField.addSubview(namePlaceholder)
-        view.addSubview(nameLine)
-        view.addSubview(nameSuccess)
-    }
-    
-    func setLayout(){
-        namePlaceholder.snp.makeConstraints { make in
-            make.leading.equalTo(0)
-            make.top.equalTo(0)
-        }
-        
-        nameTextField.snp.makeConstraints { make in
-            make.leading.equalTo(self.view.safeAreaLayoutGuide).offset(16)
-            make.trailing.equalTo(0)
-            make.top.equalTo(self.view.safeAreaLayoutGuide).offset(137)
-        }
-        
-        nameLine.snp.makeConstraints { make in
-            make.leading.equalTo(16)
-            make.trailing.equalTo(-16)
-            make.top.equalTo(self.view.safeAreaLayoutGuide).offset(170)
-            make.height.equalTo(2)
-        }
-        
-        nameSuccess.snp.makeConstraints { make in
-            make.leading.equalTo(0)
-            make.trailing.equalTo(0)
-            make.bottom.equalTo(self.view.safeAreaLayoutGuide).offset(0)
-            make.height.equalTo(48)
-        }
-    }
-    
-    func placeholderUp(text: UILabel){
-        // 애니메이션
-        text.font = UIFont(name: "NotoSansKR-Regular", size: 14)
-        text.snp.remakeConstraints {
-            $0.top.equalTo(-20)
-        }
-    }
-    
-    func lineFocus(){
-        nameLine.backgroundColor = #colorLiteral(red: 0.1137254902, green: 0.6666666667, blue: 0.9529411765, alpha: 1)
-    }
-    
-    func lineUnfocus(){
-        nameLine.backgroundColor = #colorLiteral(red: 0.6, green: 0.6, blue: 0.6, alpha: 1)
-    }
-}
-
+// MARK: -Extension
 extension SignUpVC: UITextFieldDelegate{
     func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
         if textField.tag == 1{
