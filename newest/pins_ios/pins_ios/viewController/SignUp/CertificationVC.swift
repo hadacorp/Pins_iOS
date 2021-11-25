cf065a51dc0ed4879edba421378f75a079f73314
judongseok <wnehdtjr5@naver.com>
2021-11-26 03:27:35 +0900

    Feat NicknameVC UI

--- newest/pins_ios/pins_ios/viewController/SignUp/CertificationVC.swift
+++ newest/pins_ios/pins_ios/viewController/SignUp/CertificationVC.swift
@@ -10,6 +10,10 @@ import UIKit
 
 // MARK: - ViewController Main Variable
 class CertificationVC: UIViewController, BaseViewController{
+    var timer: Timer?
+    var timeLeft = 180
+    var sampleCode = "123456"
+    
     let backBtn: UIButton = {
         let btn = UIButton()
         
@@ -18,10 +22,16 @@ class CertificationVC: UIViewController, BaseViewController{
         return btn
     }()
     
-    let titleLabel: UILabel = {
+    let titleLabel_1: UILabel = {
         let label = UILabel()
-        label.text = "문자로 전송된\n인증번호 6자리를 입력해 주세요."
-        label.numberOfLines = 2
+        label.text = "문자로 전송된"
+        label.font = UIFont(name: "NotoSansKR-Medium", size: 20)
+        label.textColor = #colorLiteral(red: 0.06666666667, green: 0.06666666667, blue: 0.06666666667, alpha: 1)
+        return label
+    }()
+    let titleLabel_2: UILabel = {
+        let label = UILabel()
+        label.text = "인증번호 6자리를 입력해 주세요."
         label.font = UIFont(name: "NotoSansKR-Medium", size: 20)
         label.textColor = #colorLiteral(red: 0.06666666667, green: 0.06666666667, blue: 0.06666666667, alpha: 1)
         return label
@@ -39,6 +49,7 @@ class CertificationVC: UIViewController, BaseViewController{
         let field = UITextField()
         field.textColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
         field.font = UIFont(name: "NotoSansKR-Regular", size: 20)
+        field.keyboardType = .numberPad
         return field
     }()
     
@@ -56,12 +67,31 @@ class CertificationVC: UIViewController, BaseViewController{
         label.underline()
         return label
     }()
+    
+    let timeLabel: UILabel = {
+        let label = UILabel()
+        label.text = "3:00"
+        label.textColor = #colorLiteral(red: 0.3764705882, green: 0.3764705882, blue: 0.3764705882, alpha: 1)
+        label.font = UIFont(name: "NotoSansKR-Regular", size: 16)
+        return label
+    }()
+    
+    let failLabel: UILabel = {
+        let label = UILabel()
+        label.text = "인증번호가 올바르지 않습니다."
+        label.textColor = #colorLiteral(red: 1, green: 0.3058823529, blue: 0.1607843137, alpha: 1)
+        label.font = UIFont(name: "NotoSansKR-Regular", size: 12)
+        label.layer.opacity = 0
+        return label
+    }()
 }
 
 // MARK: - ViewController LifeCycle
 extension CertificationVC{
     override func viewDidLoad() {
         setUI()
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(onTimerFires), userInfo: nil, repeats: true)
+        numberTextField.becomeFirstResponder()
     }
 }
 
@@ -79,11 +109,14 @@ extension CertificationVC{
     
     func addSubViews() {
         view.addSubview(backBtn)
-        view.addSubview(titleLabel)
+        view.addSubview(titleLabel_1)
+        view.addSubview(titleLabel_2)
         view.addSubview(line)
         view.addSubview(numberTextField)
         numberTextField.addSubview(placeholder)
         view.addSubview(resendBtn)
+        view.addSubview(timeLabel)
+        view.addSubview(failLabel)
     }
     
     func setLayout() {
@@ -92,10 +125,13 @@ extension CertificationVC{
             make.top.equalTo(self.view.safeAreaLayoutGuide)
             make.width.height.equalTo(48)
         }
-        titleLabel.snp.makeConstraints { make in
+        titleLabel_2.snp.makeConstraints { make in
+            make.leading.equalTo(16)
+            make.top.equalTo(self.view.safeAreaLayoutGuide).offset(104)
+        }
+        titleLabel_1.snp.makeConstraints { make in
             make.leading.equalTo(16)
             make.top.equalTo(self.view.safeAreaLayoutGuide).offset(64)
-            make.height.equalTo(69)
         }
         placeholder.snp.makeConstraints { make in
             make.leading.equalTo(0)
@@ -116,6 +152,14 @@ extension CertificationVC{
             make.trailing.equalTo(-16)
             make.top.equalTo(self.view.safeAreaLayoutGuide).offset(218)
         }
+        timeLabel.snp.makeConstraints { make in
+            make.trailing.equalTo(-16)
+            make.top.equalTo(self.view.safeAreaLayoutGuide).offset(180)
+        }
+        failLabel.snp.makeConstraints { make in
+            make.leading.equalTo(16)
+            make.top.equalTo(self.view.safeAreaLayoutGuide).offset(218)
+        }
     }
     
     func placeholderUp(text: UILabel){
@@ -126,6 +170,33 @@ extension CertificationVC{
                 $0.top.equalTo(-20)
             }
             self.view.layoutIfNeeded()
+        }
+    }
+    
+    @objc func onTimerFires(){
+        timeLeft -= 1
+        if timeLeft % 60 < 10{
+            timeLabel.text = "\(timeLeft / 60):0\(timeLeft % 60)"
+        }
+        else{
+            timeLabel.text = "\(timeLeft / 60):\(timeLeft % 60)"
+        }
+        if timeLeft <= 0 {
+            timer?.invalidate()
+            timer = nil
+        }
+    }
+    
+    func validation(text: String){
+        if text == sampleCode{
+            // 화면 전환
+            let nickVC = self.storyboard!.instantiateViewController(withIdentifier: "NicknameVC") as! NicknameVC
+            self.navigationController?.pushViewController(nickVC, animated: true)
+        }
+        else{
+            placeholder.textColor = #colorLiteral(red: 1, green: 0.3058823529, blue: 0.1607843137, alpha: 1)
+            line.backgroundColor = #colorLiteral(red: 1, green: 0.3058823529, blue: 0.1607843137, alpha: 1)
+            failLabel.layer.opacity = 1
         }
     }
 }
@@ -142,6 +213,18 @@ extension CertificationVC{
 extension CertificationVC: UITextFieldDelegate{
     func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
         placeholderUp(text: placeholder)
+        let newLength = textField.text!.count + string.count - range.length
+        if newLength > 5 && textField.text!.count == 5{
+            textField.text! += string
+            self.view.endEditing(true)
+            validation(text: numberTextField.text!)
+        }
+        else if newLength <= 5{
+            return true
+        }
+        else {
+            return false
+        }
         return true
     }
 }
