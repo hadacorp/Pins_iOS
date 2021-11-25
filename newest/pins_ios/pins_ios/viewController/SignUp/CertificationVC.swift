623e6cc1c6137df28c3f24d298d59d5b3d179a1c
MojitoBar <wnehdtjr5@gmail.com>
2021-11-26 00:16:23 +0900

    Feat CertificationVC titlelabel

--- newest/pins_ios/pins_ios/viewController/SignUp/CertificationVC.swift
+++ newest/pins_ios/pins_ios/viewController/SignUp/CertificationVC.swift
@@ -17,6 +17,15 @@ class CertificationVC: UIViewController, BaseViewController{
         btn.addTarget(self, action: #selector(backAction), for: .touchUpInside)
         return btn
     }()
+    
+    let titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "문자로 전송된\n인증번호 6자리를 입력해 주세요."
+        label.numberOfLines = 2
+        label.font = UIFont(name: "NotoSansKR-Medium", size: 20)
+        label.textColor = #colorLiteral(red: 0.06666666667, green: 0.06666666667, blue: 0.06666666667, alpha: 1)
+        return label
+    }()
 }
 
 // MARK: - ViewController LifeCycle
@@ -40,6 +49,7 @@ extension CertificationVC{
     
     func addSubViews() {
         view.addSubview(backBtn)
+        view.addSubview(titleLabel)
     }
     
     func setLayout() {
@@ -47,6 +57,11 @@ extension CertificationVC{
             make.leading.equalTo(0)
             make.top.equalTo(self.view.safeAreaLayoutGuide)
             make.width.height.equalTo(48)
+        }
+        titleLabel.snp.makeConstraints { make in
+            make.leading.equalTo(16)
+            make.top.equalTo(self.view.safeAreaLayoutGuide).offset(64)
+            make.height.equalTo(69)
         }
     }
 }
