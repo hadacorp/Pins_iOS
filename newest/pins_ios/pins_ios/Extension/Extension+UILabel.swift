5de2f6e61a78fa555a85da16157aa1f56afd5c20
MojitoBar <wnehdtjr5@gmail.com>
2021-11-26 00:44:57 +0900

    Feat certificationVC resend button

---
+++ newest/pins_ios/pins_ios/Extension/Extension+UILabel.swift
@@ -0,0 +1,19 @@
+//
+//  Extension+UILabel.swift
+//  pins_ios
+//
+//  Created by judongseok on 2021/11/26.
+//
+
+import UIKit
+extension UILabel {
+    func underline() {
+        if let textString = self.text {
+          let attributedString = NSMutableAttributedString(string: textString)
+            attributedString.addAttribute(NSAttributedString.Key.underlineStyle,
+                                          value: NSUnderlineStyle.single.rawValue,
+                                          range: NSRange(location: 0, length: attributedString.length))
+          attributedText = attributedString
+        }
+    }
+}
