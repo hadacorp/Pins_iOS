419800f86a862ddd1d59dc6aa9f1144131580497
MojitoBar <wnehdtjr5@gmail.com>
2021-11-29 23:37:40 +0900

    Modified nickvc keyboard logic

--- newest/pins_ios/pins_ios/viewController/SignUp/NicknameVC.swift
+++ newest/pins_ios/pins_ios/viewController/SignUp/NicknameVC.swift
@@ -55,8 +55,29 @@ class NicknameVC: UIViewController, BaseViewController {
     
     let nickLine: UIView = {
         let line = UIView()
-        line.backgroundColor = #colorLiteral(red: 0.9215686275, green: 0.9215686275, blue: 0.9215686275, alpha: 1)
+        line.backgroundColor = #colorLiteral(red: 0.1137254902, green: 0.6666666667, blue: 0.9529411765, alpha: 1)
         return line
+    }()
+    
+    let nickDiscription: UILabel = {
+        let text = UILabel()
+        text.text = "한글 닉네임을 권장드려요."
+        text.textColor = #colorLiteral(red: 0.1137254902, green: 0.6666666667, blue: 0.9529411765, alpha: 1)
+        text.font = UIFont(name: "NotoSansKR-Regular", size: 12)
+        return text
+    }()
+    
+    let confirmButton: UIButton = {
+        let button = UIButton()
+        
+        button.setTitle("확인", for: .normal)
+        button.titleLabel?.textAlignment = .center
+        button.titleLabel?.font = UIFont(name: "NotoSansKR-Regular", size: 16)
+        button.setTitleColor(.white, for: .normal)
+        button.backgroundColor = #colorLiteral(red: 0.1137254902, green: 0.6666666667, blue: 0.9529411765, alpha: 1)
+        
+        button.addTarget(self, action: #selector(nextView), for: .touchUpInside)
+        return button
     }()
 }
 
@@ -71,6 +92,7 @@ extension NicknameVC{
 // MARK: - ViewController Setting UI
 extension NicknameVC{
     func setUI() {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
         setDelegate()
         addSubViews()
         setLayout()
@@ -87,6 +109,8 @@ extension NicknameVC{
         view.addSubview(nickTextField)
         view.addSubview(nickLine)
         nickTextField.addSubview(nickPlaceholder)
+        view.addSubview(nickDiscription)
+        view.addSubview(confirmButton)
     }
     
     func setLayout() {
@@ -113,9 +137,47 @@ extension NicknameVC{
         }
         nickLine.snp.makeConstraints { make in
             make.leading.equalTo(16)
-            make.trailing.equalTo(16)
+            make.trailing.equalTo(-16)
             make.top.equalTo(self.view.safeAreaLayoutGuide).offset(210)
             make.height.equalTo(2)
+        }
+        nickDiscription.snp.makeConstraints { make in
+            make.leading.equalTo(16)
+            make.top.equalTo(self.view.safeAreaLayoutGuide).offset(218)
+        }
+        confirmButton.snp.makeConstraints { make in
+            make.leading.equalTo(0)
+            make.trailing.equalTo(0)
+            make.bottom.equalTo(self.view.safeAreaLayoutGuide).offset(0)
+            make.height.equalTo(48)
+        }
+    }
+    
+    func placeholderUp(text: UILabel){
+        // 애니메이션
+        text.font = UIFont(name: "NotoSansKR-Regular", size: 14)
+        UIView.animate(withDuration: 0.25, delay: 0, options: .curveEaseOut) {
+            text.snp.remakeConstraints {
+                $0.top.equalTo(-20)
+            }
+            self.view.layoutIfNeeded()
+        }
+    }
+    
+    @objc
+    func nextView(){
+        // 뷰 전환
+    }
+    
+    @objc
+    func keyboardWillShow(_ sender: Notification) {
+        if let keyboardFrame: NSValue = sender.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue {
+            let keybaordRectangle = keyboardFrame.cgRectValue
+            let keyboardHeight = keybaordRectangle.height
+            
+            confirmButton.snp.makeConstraints { make in
+                make.bottom.equalTo(-keyboardHeight)
+            }
         }
     }
 }
@@ -129,5 +191,10 @@ extension NicknameVC{
 }
 
 extension NicknameVC: UITextFieldDelegate{
-    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        if textField.tag == 0{
+            placeholderUp(text: nickPlaceholder)
+        }
+        return true
+    }
 }
