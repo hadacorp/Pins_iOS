8d7f347d4bfb0d730c8cdbfb16ed516cd3c47a2b
MojitoBar <wnehdtjr5@gmail.com>
2022-03-07 21:34:25 +0900

    Feat: JoinView Popup

--- Pins/CardDetail/CardDetailViewController+Action.swift
+++ Pins/CardDetail/CardDetailViewController+Action.swift
@@ -12,4 +12,30 @@ extension CardDetailViewController {
     func backBtn() {
         self.navigationController?.popViewController(animated: true)
     }
+    
+    func reload() {
+        if viewModel.isJoinHidden {
+            viewModel.isJoinHidden = false
+            joinBackground.setHidden(hidden: viewModel.isJoinHidden)
+            return
+        }
+        else {
+            viewModel.isJoinHidden = true
+            joinBackground.setHidden(hidden: viewModel.isJoinHidden)
+        }
+    }
+    
+    func rxSetup() {
+        joinBtn.rx.tap
+            .subscribe(onNext: { [weak self] in self?.reload() })
+            .disposed(by: disposeBag)
+        
+        cancelBtn.rx.tap
+            .subscribe(onNext: { [weak self] in self?.reload() })
+            .disposed(by: disposeBag)
+        
+        applyBtn.rx.tap
+            .subscribe(onNext: { [weak self] in self?.reload() })
+            .disposed(by: disposeBag)
+    }
 }
