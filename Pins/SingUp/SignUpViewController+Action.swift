f23eedca238500ce907761b939f7d2e5a87e9c29
MojitoBar <wnehdtjr5@naver.com>
2022-04-18 01:02:27 +0900

    feat: namePlaceholder animation

---
+++ Pins/SingUp/SignUpViewController+Action.swift
@@ -0,0 +1,39 @@
+//
+//  SignUpViewController+Action.swift
+//  Pins
+//
+//  Created by judongseok on 2022/04/18.
+//
+
+import UIKit
+
+extension SignUpViewController {
+    func namePlaceholderAnimation(isTyping: Bool) {
+        if isTyping {
+            UIView.animate(withDuration: 0.2, delay: 0) { [self] in
+                let anchorPoint = CGPoint(x: 0.75, y: 0.5)
+                namePlaceholder.layer.anchorPoint = anchorPoint
+                namePlaceholder.transform = CGAffineTransform(scaleX: 0.7, y: 0.7)
+                namePlaceholder.snp.updateConstraints {
+                    $0.bottom.equalToSuperview().offset(-31)
+                    $0.leading.equalTo(0)
+                    $0.height.equalTo(20)
+                }
+                namePlaceholder.superview?.layoutIfNeeded()
+            }
+        }
+        else {
+            UIView.animate(withDuration: 0.2, delay: 0) { [self] in
+                let anchorPoint = CGPoint(x: 0.5, y: 0.5)
+                namePlaceholder.layer.anchorPoint = anchorPoint
+                namePlaceholder.transform = CGAffineTransform(scaleX: 1.0, y: 1.0)
+                namePlaceholder.snp.updateConstraints {
+                    $0.bottom.equalToSuperview()
+                    $0.leading.equalTo(0)
+                    $0.height.equalTo(37)
+                }
+                namePlaceholder.superview?.layoutIfNeeded()
+            }
+        }
+    }
+}
--- Pins/SingUp/SignUpViewController.swift
+++ Pins/SingUp/SignUpViewController.swift
@@ -9,8 +9,14 @@ import Foundation
 import UIKit
 
 class SignUpViewController: BaseViewController {
+    // public variable
+    var nameTextField: CustomTextField!
+    var namePlaceholder: CustomLabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        nameTextField.becomeFirstResponder()
     }
     
     override func setupUI() {
@@ -22,5 +28,53 @@ class SignUpViewController: BaseViewController {
                 $0.leading.equalTo(16)
             }
             .setImage(image: UIImage(named: "back")!)
+        
+        CustomLabel(parent: self.view)
+            .makeConstraints {
+                $0.top.equalTo(self.view.safeAreaLayoutGuide).offset(67)
+                $0.leading.equalTo(16)
+            }
+            .setText(text: "이름을 입력해 주세요.")
+            .setFont(name: "NotoSansKR-Medium", size: 20)
+            .setColor(color: UIColor.black)
+        
+        nameTextField = CustomTextField(parent: self.view)
+            .makeConstraints{
+                $0.leading.equalTo(16)
+                $0.trailing.equalTo(-16)
+                $0.top.equalTo(self.view.safeAreaLayoutGuide).offset(140)
+                $0.height.equalTo(37)
+            }
+            .setCursorColor(color: UIColor(hex: "1DAAF3"))
+            .setTextColor(color: UIColor.black)
+            .setFont(name: "NotoSansKR-Regular", size: 16)
+            .setDelegate(delegate: self)
+        
+        namePlaceholder = CustomLabel(parent: nameTextField)
+            .makeConstraints { [self] in
+                // $0.bottom.equalTo(nameTextField.snp.top).offset(1)
+                $0.bottom.equalTo(nameTextField.snp.bottom)
+                $0.leading.equalTo(0)
+                $0.height.equalTo(37)
+            }
+            .setFont(name: "NotoSansKR-Regular", size: 20)
+            .setText(text: "이름")
+            .setColor(color: UIColor.init(hex: "999999"))
+        
+        CustomView(parent: self.view)
+            .makeConstraints {
+                $0.leading.equalTo(16)
+                $0.trailing.equalTo(-16)
+                $0.top.equalTo(self.view.safeAreaLayoutGuide).offset(172)
+                $0.height.equalTo(2)
+            }
+            .setColor(color: UIColor.init(hex: "1DAAF3"))
+    }
+}
+
+extension SignUpViewController: UITextFieldDelegate {
+    func textFieldDidChangeSelection(_ textField: UITextField) {
+        // 애니메이션 작동
+        namePlaceholderAnimation(isTyping: textField.text == "" ? false : true)
     }
 }
